38e7b4bc0724c85227063a5cdcf363c5616ae35d
Parviz <alievz2011@gmail.com>
2021-12-17 21:46:34 +0300

    Scripted auto-commit on change (2021-12-17 21:46:34) by gitwatch.sh

---
+++ src/main/java/lab2/App.java
@@ -0,0 +1,13 @@
+package lab2;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class App {
