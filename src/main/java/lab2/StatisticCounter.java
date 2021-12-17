400635582678021ec0f51dfd439ee7d00abde663
Parviz <alievz2011@gmail.com>
2021-12-17 22:20:01 +0300

    Scripted auto-commit on change (2021-12-17 22:20:01) by gitwatch.sh

--- src/main/java/lab2/StatisticCounter.java
+++ src/main/java/lab2/StatisticCounter.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 public class StatisticCounter extends Reducer<AirportWritableComparable, FlightWritable, IntWritable, Text> {
+    private static final String format = "Airport:%s  Average_delay:%f, Minimum_delay %f, Maximum_delay:%f, Total_flights:%d";
+
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<FlightWritable> values, Context context) throws IOException, InterruptedException {
         float averageDelay = 0;
