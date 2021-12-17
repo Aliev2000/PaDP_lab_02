6cfcedab11d7768720d9a2a9a487e8b59c6562b6
Parviz <alievz2011@gmail.com>
2021-12-17 22:15:16 +0300

    Scripted auto-commit on change (2021-12-17 22:15:16) by gitwatch.sh

--- src/main/java/lab2/FlightPartitioner.java
+++ src/main/java/lab2/FlightPartitioner.java
@@ -4,4 +4,5 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightPartitioner extends Partitioner<AirportWritableComparable, FlightWritable> {
     public int getPartition(AirportWritableComparable key, FlightWritable val, int i) {
+        return key.getId() % i;
 }
