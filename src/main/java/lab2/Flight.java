6fb0c2f1be79c53ac7641e98e607e6fd92df265b
Parviz <alievz2011@gmail.com>
2021-12-17 21:50:17 +0300

    Scripted auto-commit on change (2021-12-17 21:50:17) by gitwatch.sh

--- src/main/java/lab2/Flight.java
+++ src/main/java/lab2/Flight.java
@@ -3,6 +3,7 @@ package lab2;
 public class Flight{
     private static final int DELAY_TIME_COLUMN = 18;
     private static final String DELIMITER = ",";
+    private final float delayTime;
     public boolean isCancelled() {
         return isCancelled;
 
