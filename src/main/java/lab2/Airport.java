d564146108a1ec748628371210b24507dc5073a8
Parviz <alievz2011@gmail.com>
2021-12-17 22:02:45 +0300

    Scripted auto-commit on change (2021-12-17 22:02:45) by gitwatch.sh

--- src/main/java/lab2/Airport.java
+++ src/main/java/lab2/Airport.java
@@ -2,6 +2,7 @@ package lab2;
 
 public class Airport {
     private static final String DELIMITER = ",";
+    private final int id;
     public Airport(String s){
         String[] data = s.split(DELIMITER);
         this.name = data[1];
