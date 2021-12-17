ed4c868f5aee6e91af0b7c74f1adc76b30a03e7e
Parviz <alievz2011@gmail.com>
2021-12-17 22:16:58 +0300

    Scripted auto-commit on change (2021-12-17 22:16:58) by gitwatch.sh

--- src/main/java/lab2/FlightComparator.java
+++ src/main/java/lab2/FlightComparator.java
@@ -5,4 +5,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class FlightComparator extends WritableComparator {
     public int compare(WritableComparable a, WritableComparable b) {
+        int a_id = ((AirportWritableComparable)a).getId();
+        int b_id = ((AirportWritableComparable)b).getId();
+        return a_id - b_id;
 }
