b3e2651b4d6feb81723b9c57141c64a227d9c50d
Parviz <alievz2011@gmail.com>
2021-12-17 22:12:21 +0300

    Scripted auto-commit on change (2021-12-17 22:12:21) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -8,3 +8,5 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, FlightWritable> {
 
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        Airport data =  new Airport(value.toString());
