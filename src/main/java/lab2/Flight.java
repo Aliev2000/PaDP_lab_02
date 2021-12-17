0e5e8eda272d62b3070cc7672dceeaa5a1d7ba54
Parviz <alievz2011@gmail.com>
2021-12-17 21:51:50 +0300

    Scripted auto-commit on change (2021-12-17 21:51:50) by gitwatch.sh

--- src/main/java/lab2/Flight.java
+++ src/main/java/lab2/Flight.java
@@ -2,6 +2,8 @@ package lab2;
 
 public class Flight{
     private static final int DELAY_TIME_COLUMN = 18;
+    private static final int AIRPORT_ID_COLUMN = 14;
+
     private static final String DELIMITER = ",";
     private final float delayTime;
     private final int airportId;
