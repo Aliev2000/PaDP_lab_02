6f8bbd84b2a55648325552b463ac426d9c2a6649
Parviz <alievz2011@gmail.com>
2021-12-17 22:13:06 +0300

    Scripted auto-commit on change (2021-12-17 22:13:06) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, FlightWritable> {
 
+    @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         Airport data =  new Airport(value.toString());
     }
---
+++ src/main/java/lab2/FlightsMapper.java
@@ -0,0 +1,10 @@
+package lab2;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightsMapper extends Mapper<LongWritable, Text, AirportWritableComparable, FlightWritable> {
+
